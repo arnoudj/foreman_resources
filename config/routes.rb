2265c15c1cb0002692e0307f8e6b37e74f075924
Arnoud de Jonge <arnoud.dejonge@nxs.nl>
2013-06-24 15:52:27 +0200

    Test code removed.

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
       resources :hosts,             :except => [:new, :edit] do
         resources :puppetclasses,   :only => :index do
           resources :types,         :only => [:index, :show] do
-            #get 'resources/*x' => 'resources#show', :format => false
-            #put 'resources/*x' => 'resources#update', :format => false
-            #delete 'resources/*x' => 'resources#delete', :format => false
             resources :resources,   :id => /.+/, :except => [:new]
           end
         end
